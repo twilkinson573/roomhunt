555897a5f141d9c7173bf66c2dfc8aaee0dcbdec
ThomasMAC <tuds_wilkinson@hotmail.com>
2015-06-28 17:40:05 +0200

    Initialise Google Map through JS API

---
+++ app/assets/javascripts/map/google_map.js
@@ -0,0 +1,28 @@
+$(window).load(function() {
+      loadScript();
+});
+
+var map;
+
+function initialize() {
+  var mapOptions = {
+      center: new google.maps.LatLng(52.364534, 4.898992),
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.NORMAL,
+      panControl: true,
+      scaleControl: false,
+      streetViewControl: true,
+      overviewMapControl: true
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+  '&libraries=drawing'+
+  '&callback=initialize';
+  document.body.appendChild(script);
+}
